6ef5c3bd62b51bd2bad0702a874ba2ee16713451
HAKIZAYEZU Daniel <90722447+dannydrance@users.noreply.github.com>
2023-01-19 22:05:19 -0800

    Next we will make simple calculator

---
+++ add.py
@@ -0,0 +1,31 @@
+# Hello python beginner
+# Here is program that will return the summetion
+# of two number
+'''Let's Go
+           oooo'''
+
+# No module needed
+
+# first We creating function to enter number
+
+x=input("Enter your first number: ")
+
+y=input("Enter your second number: ")
+
+# Wait we input string valuable in python
+# we need to convert it to integer
+
+addition=int(x)+int(y)
+
+# when we add like this addition=(x)+(y) and x=1 and y=2
+# we will get 12 which is long
+
+print("Your Summation is: ", addition)
+
+''' As you need to add float 
+you will replace int by float
+
+      Thank you!!! '''
+
+
+
